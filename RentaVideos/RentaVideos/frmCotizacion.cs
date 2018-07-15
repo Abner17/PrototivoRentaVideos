35c499c3308c7162bff50f4ad8521f64eeec3a3a
Abner17 <ajuarezh1@miumg.edu.gt>
2018-07-15 12:26:00 -0600

    Formulario de Renta videos y Cotizacion

---
+++ RentaVideos/RentaVideos/frmCotizacion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RentaVideos.Formularios
+{
+    public partial class frmCotizacion : Form
+    {
+        public frmCotizacion()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCotizacion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_cerrar_Click(object sender, EventArgs e)
+        {
+            this.SetVisibleCore(false);
+        }
+    }
+}
